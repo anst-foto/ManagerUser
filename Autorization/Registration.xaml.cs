6d186c3ff45b3fd97e0552a06b5c3eec5789885a
AnSt <starininandrey@gmail.com>
2021-04-29 19:52:44 +0300

    Добавление обработчиков событий

---
+++ Autorization/Registration.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Autorization
+{
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void HyperSignIn_OnClick(object sender, RoutedEventArgs e)
+        {
+            var auth = new MainWindow();
+            auth.Show();
+            
+            Close();
+        }
+
+        private void ButtonCancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
