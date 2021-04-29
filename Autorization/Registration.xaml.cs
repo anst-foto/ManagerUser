afa1f2976241148bbe013285e3698b144544d161
AnSt <starininandrey@gmail.com>
2021-04-29 20:11:41 +0300

    Обработка события TextChanged

--- Autorization/Registration.xaml.cs
+++ Autorization/Registration.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Autorization
@@ -26,6 +28,43 @@ namespace Autorization
         private void ButtonCancel_OnClick(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void CheckEmpty()
+        {
+            var loginIsEmpty = InputLogin.Text != string.Empty;
+            var password_1_IsEmpty = InputPassword1.Password != string.Empty;
+            var password_2_IsEmpty = InputPassword2.Password != string.Empty;
+            var emailIsEmpty = InputEmail.Text != string.Empty;
+            
+            if (loginIsEmpty && password_1_IsEmpty && password_2_IsEmpty && emailIsEmpty && InputPassword1.Password == InputPassword2.Password)
+            {
+                ButtonRegistration.IsEnabled = true;
+            }
+            else
+            {
+                ButtonRegistration.IsEnabled = false;
+            }
+        }
+
+        private void InputLogin_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckEmpty();
+        }
+
+        private void InputPassword1_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            CheckEmpty();
+        }
+
+        private void InputPassword2_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            CheckEmpty();
+        }
+
+        private void InputEmail_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckEmpty();
         }
     }
 }
